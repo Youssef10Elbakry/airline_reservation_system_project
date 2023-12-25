4e31a8f9b2818a8689791effc79fecba0bd95d69
Youssef10Elbakry <97008160+Youssef10Elbakry@users.noreply.github.com>
2023-12-25 23:40:17 +0200

    finished big part of the select seat screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,31 @@
 import 'package:airline_reservation_system/ui/Auth/Login/login_screen.dart';
+import 'package:airline_reservation_system/ui/main_screen/main_screen.dart';
+import 'package:airline_reservation_system/ui/main_screen/tabs/available_tickets/select_seat/select_seat_screen.dart';
+import 'package:airline_reservation_system/ui/provider/available_tickets_provider.dart';
+import 'package:provider/provider.dart';
 import 'ui/home_screen.dart';
 import 'ui/Auth/Registeration/registeration_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(MaterialApp(
-    routes: {
-      HomeScreen.screenName: (_) =>  const HomeScreen(),
-      RegisterationScreen.screenName: (_) => RegisterationScreen(),
-      LoginScreen.screenName: (_) =>LoginScreen(),
-    },
-    initialRoute: HomeScreen.screenName,
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_)=>ATProvider()),
+    ],
+    child: MaterialApp(
+      routes: {
+        HomeScreen.screenName: (_) =>  const HomeScreen(),
+        RegisterationScreen.screenName: (_) => RegisterationScreen(),
+        LoginScreen.screenName: (_) =>LoginScreen(),
+        MainScreen.screenName: (_) => MainScreen(),
+        SelectSeatScreen.screenName: (_) => SelectSeatScreen()
+      },
+      initialRoute: MainScreen.screenName,
+    ),
   )
   );
 }
